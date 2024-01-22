a633c1647ab3d5fdcdea802cf79c38334fcec1fe
Jose Antonio P <120035990@upq.edu.mx>
2024-01-21 22:13:40 -0600

    spinner agregado, app terminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import cryptoBg from './assets/bg-2.png'
 
 const Contenedor = styled.div`
@@ -50,14 +51,18 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${monedas.criptomoneda}&tsyms=${monedas.moneda}`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
         setResultado(resultado.DISPLAY[monedas.criptomoneda][monedas.moneda])
+        setCargando(false)
       }
       cotizarCripto()
     }
@@ -74,6 +79,7 @@ function App() {
         <Formulario
           setMonedas={setMonedas}
         />
+        {cargando && <Spinner />}
         {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
